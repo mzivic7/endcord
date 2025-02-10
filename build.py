b8d8d146785b1e766b3e45f581a02c2590eb7de4
Marko Zivic <marko.b.zivic@gmail.com>
2025-02-10 05:53:11 +0100

    Fix hidden import when building

--- build.py
+++ build.py
@@ -1,14 +1,28 @@
+import importlib.util
 import os
 import sys
 
+support_media = (
+    importlib.util.find_spec("PIL") is not None and
+    importlib.util.find_spec("av") is not None and
+    importlib.util.find_spec("pyaudio") is not None
+)
+
+if support_media:
+    hidden_imports = "--hidden-import uuid "
+    print("Media support is enabled")
+else:
+    hidden_imports = ""
+    print("Media support is disabled")
+
 if sys.platform == "win32":
-    command = 'pipenv run python -m PyInstaller --noconfirm --onefile --windowed --clean --name "endcord" "main.py"'
+    command = f'pipenv run python -m PyInstaller {hidden_imports}--noconfirm --onefile --windowed --clean --name "endcord" "main.py"'
     os.system(command)
 elif sys.platform == "linux":
-    command = 'pipenv run python -m PyInstaller --noconfirm --onefile --windowed --clean --name "endcord" "main.py"'
+    command = f'pipenv run python -m PyInstaller {hidden_imports}--noconfirm --onefile --windowed --clean --name "endcord" "main.py"'
     os.system(command)
 elif sys.platform == "mac":
-    command = 'pipenv run python -m PyInstaller --noconfirm --onefile --windowed --clean --name "endcord" "main.py"'
+    command = f'pipenv run python -m PyInstaller {hidden_imports}--noconfirm --onefile --windowed --clean --name "endcord" "main.py"'
     os.system(command)
 else:
     sys.exit(f"This platform is not supported: {sys.platform}")
