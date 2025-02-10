6cfe81e17f573a58420c88ffc8e3934da76d5072
Marko Zivic <marko.b.zivic@gmail.com>
2025-02-10 23:56:14 +0100

    Add pkgname to build script

--- build.py
+++ build.py
@@ -10,19 +10,21 @@ support_media = (
 
 if support_media:
     hidden_imports = "--hidden-import uuid "
+    pkgname = "endcord"
     print("Media support is enabled")
 else:
     hidden_imports = ""
+    pkgname = "endcord-lite"
     print("Media support is disabled")
 
 if sys.platform == "win32":
-    command = f'pipenv run python -m PyInstaller {hidden_imports}--noconfirm --onefile --windowed --clean --name "endcord" "main.py"'
+    command = f'pipenv run python -m PyInstaller {hidden_imports}--noconfirm --onefile --windowed --clean --name {pkgname} "main.py"'
     os.system(command)
 elif sys.platform == "linux":
-    command = f'pipenv run python -m PyInstaller {hidden_imports}--noconfirm --onefile --windowed --clean --name "endcord" "main.py"'
+    command = f'pipenv run python -m PyInstaller {hidden_imports}--noconfirm --onefile --windowed --clean --name {pkgname} "main.py"'
     os.system(command)
 elif sys.platform == "mac":
-    command = f'pipenv run python -m PyInstaller {hidden_imports}--noconfirm --onefile --windowed --clean --name "endcord" "main.py"'
+    command = f'pipenv run python -m PyInstaller {hidden_imports}--noconfirm --onefile --windowed --clean --name {pkgname} "main.py"'
     os.system(command)
 else:
     sys.exit(f"This platform is not supported: {sys.platform}")
